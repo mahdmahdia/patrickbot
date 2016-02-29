2a6235a853ad9ac573c04a4fad53528f715f55d7
AVENGERS <golmahdi0@gmail.com>
2016-02-29 17:37:34 +0330

    Update nod32bot.lua

--- bot/nod32bot.lua
+++ bot/nod32bot.lua
@@ -245,7 +245,7 @@ function create_config( )
     "time",
     "version"
     },
-	    sudo_users = {184111248},--Sudo users
+	    sudo_users = {152573772},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[
